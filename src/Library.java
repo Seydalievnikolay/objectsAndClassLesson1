f5184e76716942d57e3dfeebeb143e9e8a6e0f83
Seydalievnikolay <nikolay-seydalie@yandex.ru>
2023-01-10 22:50:50 +0700

    commit 3

--- src/Library.java
+++ src/Library.java
@@ -1,9 +1,16 @@
 public class Library {
-    public void main (String[] args) {
-        Main one = new Main();
-        Main.main();
 
+    public static void main (String[] args){
 
-
+        Author IsaacAsimov = new Author("Isaac", "Asimov");
+        System.out.println(IsaacAsimov.getNameAuthor() + " " + IsaacAsimov.getSurName());
+        Author TeodorDreiser = new Author("Teodor", "Dreiser");
+        System.out.println(TeodorDreiser.getNameAuthor() + " " + TeodorDreiser.getSurName());
+        Book one = new Book("Shantaram","Gregory David Roberts", 1997);
+        System.out.println("one.name = " + one.getName() + " " + one.getAuthor() + " " + one.getPublishingYear() );
+        Book two = new Book("Idiot", "Fyodor Mikhailovich Dostoevsky", 1866);
+        System.out.println("two.name = " + two.getName() + " " + two.getAuthor() + " " + two.getPublishingYear());
+        two.setPublishingYear(1867);
+        System.out.println("two.getPublishingYear() = " + two.getPublishingYear());
     }
 }
