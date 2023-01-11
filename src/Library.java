0e7318a9dd0c480b5be80c39dbc5b7f239c83770
Seydalievnikolay <nikolay-seydalie@yandex.ru>
2023-01-11 19:24:56 +0700

    commit 6

--- src/Library.java
+++ src/Library.java
@@ -2,15 +2,16 @@ public class Library {
 
     public static void main (String[] args){
         Author author = new Author("Isaac", "Asimov");
+        Author secondAuthor = new Author("Fyodor", "Dostoevsky");
         Author IsaacAsimov = new Author("Isaac", "Asimov");
         System.out.println(IsaacAsimov.getNameAuthor() + " " + IsaacAsimov.getSurName());
         Author TeodorDreiser = new Author("Teodor", "Dreiser");
         System.out.println(TeodorDreiser.getNameAuthor() + " " + TeodorDreiser.getSurName());
-        Book one = new Book("Shantaram","Gregory David Roberts" , 1997);
+        Book one = new Book("Shantaram",author , 1997);
         System.out.println("Первая книга = " + one.getName() + " " + one.getAuthor() + " " + one.getPublishingYear() );
-        Book two = new Book("Idiot", "Fyodor Mikhailovich Dostoevsky", 1866);
-        System.out.println("Вторая книга = " + two.getName() + " " + two.getAuthor() + " " + two.getPublishingYear());
+        Book two = new Book("Idiot", secondAuthor, 1866);
+        System.out.println("Вторая книга = " + two.getName() + " " + secondAuthor + " " + two.getPublishingYear());
         two.setPublishingYear(1867);
-        System.out.println("Вторая книга = " + two.getName() + " " + two.getAuthor() + "" + two.getPublishingYear());
+        System.out.println("Вторая книга = " + two.getName() + " " + secondAuthor + " " + two.getPublishingYear());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main() {
         Author author = new Author("Isaac", "Asimov");
-
-        Book one = new Book("Shantaram", "Isaac Asimov", 1997);
+        Author secondAuthor = new Author("Fyodor", "Dostoevsky");
+        Book one = new Book("Shantaram", author, 1997);
         System.out.println("one.name = " + one.getName() + " " + one.getAuthor() + " " + one.getPublishingYear() );
-        Book two = new Book("Idiot", Fyodor Mikhailovich Dostoevsky, 1866);
+        Book two = new Book("Idiot", secondAuthor, 1866);
         System.out.println("two.name = " + two.getName() + " " + two.getAuthor() + " " + two.getPublishingYear());
         two.setPublishingYear(1867);
         System.out.println("two.getPublishingYear() = " + two.getPublishingYear());
